4c51c962d1e6d685135089eb1e303d893e9b1e65
Deepak Rout <deepakrout@gmail.com>
2019-09-12 22:26:56 -0400

    Added View

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,18 @@
-import  React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is the components screen</Text>
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
-    }
+  textStyle: {
+    fontSize: 30
+  }
 });
 
 export default ComponentsScreen;
